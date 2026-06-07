ee3cd4d768f8d9fdea9a81c36972c380a457463a
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-06-07 18:51:12 +0300

    Initial commit

--- lib/core/presentation/states/loaded_states.dart
+++ lib/core/presentation/states/loaded_states.dart
@@ -1,15 +1,6 @@
 import 'base/main_loaded_state.dart';
 
 
-class SingleModelSuccessState<T> extends LoadedState {
-  T? firstModel;
-
-  SingleModelSuccessState({
-    required this.firstModel
-  });
-}
-
-
 class DoubleModelSuccessState<T, U> extends LoadedState {
   T? firstModel;
   U? secondModel;
@@ -17,18 +8,6 @@ class DoubleModelSuccessState<T, U> extends LoadedState {
   DoubleModelSuccessState({
     required this.firstModel,
     required this.secondModel
-  });
-}
-
-class TripleModelSuccessState<T, U, S> extends LoadedState {
-  T? firstModel;
-  U? secondModel;
-  S? thirdModel;
-
-  TripleModelSuccessState({
-    required this.firstModel,
-    required this.secondModel,
-    required this.thirdModel
   });
 }
 
