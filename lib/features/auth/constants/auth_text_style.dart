87fc9881cf04e8483767a79c558bec21c4d3ef02
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-08-21 15:12:51 +0300

    Initial commit

--- lib/features/auth/constants/auth_text_style.dart
+++ lib/features/auth/constants/auth_text_style.dart
@@ -4,9 +4,9 @@ import 'package:test_app/core/constants/app_sizes.dart';
 
 abstract class AuthTextStyles {
   static const TextStyle textStyle_16 = TextStyle(
-    fontSize: AppSizes.sm,
     letterSpacing: 1.2,
     color: Colors.white,
+    fontSize: AppSizes.sm,
     fontWeight: FontWeight.bold,
   );
 }
